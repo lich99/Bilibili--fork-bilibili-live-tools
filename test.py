1de2fa719cc4a6632ba298523574a60b2452d5dd
CH.Li <32587096+lich99@users.noreply.github.com>
2019-11-18 12:59:15 +0800

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Nov 16 18:53:13 2019
-
-@author: Chenghai Li
-"""
-with open(r'./stat/daliy_total.txt','r') as f:
-    amount = f.readline()
-print(amount)
